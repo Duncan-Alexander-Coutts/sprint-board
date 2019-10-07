8d611a49996f6f6f92c7719601d2f225208c844a
duncan coutts <duncan.coutts@juralio.com>
2019-10-07 12:39:34 +0100

    Adding in batch file to temporarily run from windows in the team room

--- frontend/src/service/issue-service.js
+++ frontend/src/service/issue-service.js
@@ -1,5 +1,4 @@
-//const BASE_PATH = "https://juralio.atlassian.net/rest/agile/1.0";
-const BASE_PATH = "http://192.168.9.47:3001";
+const BASE_PATH = "http://localhost:3001";
 
 export const getCurrentSprint = () =>
   fetch(`${BASE_PATH}/currentsprint`).then(response => response.json());
---
+++ sprint-board.bat
@@ -0,0 +1,5 @@
+git pull
+cd frontend && call npm install && cd ..
+cd backend && call npm install && cd ..
+start /D backend npm start
+start /D frontend npm start
